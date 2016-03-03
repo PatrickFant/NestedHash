1a09d7f66054d213d7cf0b33de9b0eacb665c4e8
PatrickFant <pbfant@bu.edu>
2016-03-03 13:57:41 -0500

    Rewrote NestedHash class to extend Forwardable.

--- lib/nestedhash.rb
+++ lib/nestedhash.rb
@@ -1,27 +1,12 @@
-class NestedHash
-  include Enumerable
+require 'forwardable'
 
+class NestedHash
+  extend Forwardable
+  include Enumerable
+  
+  def_delegators :@outer, :[], :[]=, :keys, :values, :each
+  
   def initialize
     @outer = Hash.new { |hash, key| hash[key] = Hash.new(&hash.default_proc) }
-  end
-  
-  def keys
-    @outer.keys
-  end
-  
-  def values
-    @outer.values
-  end
-  
-  def [](key)
-    @outer[key]
-  end
-  
-  def []=(key, value)
-    @outer[key] = value
-  end
-  
-  def each
-    @outer.each { |key, value| yield(key, value) }
   end
 end
